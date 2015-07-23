02aeba62e8a013d76a1272369ae09b062e855cdd
Code-Cola <code_cola@163.com>
2015-07-24 02:10:51 +0800

    add base.html footer

---
+++ web/core/templatetags/tags.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+#!/usr/bin/python
+
+from django import template
+import datetime
+
+register = template.Library()
+@register.tag(name='year')
+def do_year(parser, token):
+	try:
+		tag_name, format_string = token.split_contents()
+	except ValueError:
+		raise template.TemplateSyntaxError()
+	return CurrentTimeNode(format_string[1:-1])
--- web/web/settings.py
+++ web/web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'bootstrap3',
     'core',
+    'users',
 )
 
 MIDDLEWARE_CLASSES = (
