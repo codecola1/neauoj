a7dfbaf4007522c4a786ef77deb171db64c8cbba
Code-Cola <code_cola@163.com>
2015-08-01 23:13:43 +0800

    userpage框架

---
+++ web/users/templatetags/user_tags.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+#!/usr/bin/python
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='school')
+def school(s):
+    l = {
+        'neau':u'东北农业大学',
+        'other':u'其他',
+    }
+    return l[s]
+
+@register.filter(name='team')
+def team(k):
+    if k:
+        return 'Yes'
+    return 'No'
